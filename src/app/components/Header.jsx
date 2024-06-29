c16479aa42117f985e994b43f187f53690cd5b8e
nrbnayon <nrbnayon@gmail.com>
2024-06-30 05:35:24 +0600

    Nav responsive

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -9,52 +9,12 @@ import {
 } from "react-icons/hi";
 import { MdExplore } from "react-icons/md";
 import { FaRegStar } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
 
 const Header = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <details>
-                <summary className="font-semibold">
-                  <MdExplore className="text-[#8064A2]  fill-[#8064A2] text-xl" />{" "}
-                  Explore
-                </summary>
-                <ul className="p-2"></ul>
-              </details>
-            </li>
-            <li>
-              <details>
-                <summary className="font-semibold">
-                  <FaRegStar className="text-[#8064A2]  fill-[#8064A2] text-xl" />
-                  Hobbies
-                </summary>
-                <ul className="p-2"></ul>
-              </details>
-            </li>
-          </ul>
-        </div>
         <a className=" text-xl">
           <Image src={Logo} alt="Logo" width={150} height={150} />
         </a>
@@ -73,7 +33,7 @@ const Header = () => {
             <CiSearch className="text-white h-5 w-5" />
           </button>
         </label>
-        <ul className="menu menu-horizontal px-1 flex">
+        <ul className="menu menu-horizontal px-1 hidden md:flex">
           <li>
             <details className="relative">
               <summary className="font-semibold">
@@ -186,10 +146,15 @@ const Header = () => {
       </div>
 
       <div className="navbar-end flex items-center">
-        <div className="flex space-x-1">
-          <div className="relative">
+        <div className="flex space-x-0 md:space-x-1">
+          <div className="relative hidden md:inline-block">
             <button className="btn btn-sm btn-ghost">
               <HiOutlineBookmark className="h-6 w-6 text-[#8064A2] fill-[#8064A2]" />
+            </button>
+          </div>
+          <div className="relative  md:hidden">
+            <button type="button" className="btn btn-sm btn-ghost">
+              <CiSearch className="h-6 w-6 text-[#8064A2] fill-[#8064A2]" />
             </button>
           </div>
           <div className="relative">
@@ -197,13 +162,19 @@ const Header = () => {
               <HiOutlineBell className="h-6 w-6 text-[#8064A2] fill-[#8064A2]" />
             </button>
           </div>
-          <div className="relative">
+          <div className="relative  md:hidden">
+            <button className="btn btn-sm btn-ghost">
+              <IoMdMenu className="h-6 w-6 text-[#8064A2] fill-[#8064A2]" />
+            </button>
+          </div>
+          <div className="relative hidden md:inline-block">
             <button className="btn btn-sm btn-ghost">
               <HiOutlineShoppingCart className="h-6 w-6 text-[#8064A2] fill-[#8064A2]" />
             </button>
           </div>
         </div>
-        <a className="inline-block ml-2 py-2 px-4 border border-[#8064A2]  text-[#8064A2] font-semibold rounded-lg">
+
+        <a className="hidden md:inline-block ml-2 py-2 px-4 border border-[#8064A2]  text-[#8064A2] font-semibold rounded-lg">
           Sign In
         </a>
       </div>
